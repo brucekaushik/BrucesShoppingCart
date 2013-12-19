e60a6f50e735ca9d21605a855a0acc3d8241b6cc
Kaushik <kaushik.srinivas@gmail.com>
2013-12-19 15:25:33 +0530

    updated index.php to authenticate user with BrucesAdminArea and then redirect here; set app_name session variable'

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
+session_start();
+
+$_SESSION['verify_back_to'] = "../BrucesShoppingCart/index2.php";
+$_SESSION['app_name'] = "Bruce's Shopping Cart";
+
 // redirect to home page
-header ("Location: ../08-adminArea/index.php");
+header ("Location: ../BrucesAdminArea/index.php?action=VerifyLogin");
 
 ?>
